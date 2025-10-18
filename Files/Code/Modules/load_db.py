5faba8c66939f2dac8962eef48057b4ae76da5df
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2025-10-18 19:58:21 -0300

    upgrading to load from file instead of loading everytime

--- Files/Code/Modules/load_db.py
+++ Files/Code/Modules/load_db.py
@@ -1,10 +1,11 @@
 """ Loading DB """
 
+import os
 import pandas as pd
 import datasets as ds
 # from ds import load_dataset
 
-def get_datasets(selected_df: str) -> ds.dataset_dict.DatasetDict:
+def get_dataset(selected_df: str) -> ds.dataset_dict.DatasetDict:
     """ selected_df options: 'ae-110k', 'oa-mine', 'mave' """
 
     def get_file_name(segment: str) -> str:
@@ -25,23 +26,51 @@ def get_datasets(selected_df: str) -> ds.dataset_dict.DatasetDict:
         loaded_df = pd.read_parquet(url)
         return loaded_df
 
+    def load_from_path(saved_df_path: str) -> ds.dataset_dict.DatasetDict:
+        """ Loads the dataset from local path """
+        dfs = ds.load_from_disk(saved_df_path)
+        return dfs
+
     def load_dataset_from_hf(dataset: str) -> ds.dataset_dict.DatasetDict:
         """ Loads the dataset from Hugging Face """
         dataset = ds.load_dataset(f'av-generation/{dataset}-dataset')
         return dataset
 
+    def get_path_to_save(selected_df: str = '') -> str:
+        """ Returns the path of the running code file """
+        # current_path = os.path.abspath(__file__)
+        saving_path = 'JSONLLM/Files/Code/Datasets/'
+        normalized_path = os.getcwd().split('JSONLLM')[0] + saving_path
+        normalized_path = os.path.normpath(normalized_path)
+        normalized_path = os.path.join(normalized_path, selected_df)
+        return normalized_path
+
+    def save_to_path(dfs: ds.dataset_dict.DatasetDict, saving_path: str) -> None:
+        """ Saves the dataset to local path """
+        # print(f'Saving dataset to "{normalized_path}"...')
+        dfs.save_to_disk(saving_path, max_shard_size='100MB')
+
     dfs = {
         'train': pd.DataFrame(),
-        'test': pd.DataFrame()
+        'test': pd.DataFrame(),
+        'validation': pd.DataFrame(),
     }
 
     dataset_options = ['ae-110k', 'oa-mine', 'mave'] # Check if mave works
-
+    df_saving_path = get_path_to_save(selected_df)
     if selected_df in dataset_options:
-        dfs = load_dataset_from_hf(selected_df)
+        if os.path.exists(df_saving_path):
+            dfs = load_from_path(df_saving_path)
+        else:
+            dfs = load_dataset_from_hf(selected_df)
+            save_to_path(dfs, df_saving_path)
         # dfs['train'] = load_parquet(selected_df, 'train')
         # dfs['test'] = load_parquet(selected_df, 'test')
 
     return dfs
 
-# ae110k = get_datasets('ae-110k')
+# datasets_dict = {
+#     'ae-110k': get_dataset('ae-110k'),
+#     'oa-mine': get_dataset('oa-mine'),
+#     'mave': get_dataset('mave'),
+# }
