85738cb0ed98c5c9587f0815ef53a7dc7a26b033
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2025-12-04 17:52:00 -0300

    Charting

--- Files/Code/Modules/plotting_charts.py
+++ Files/Code/Modules/plotting_charts.py
@@ -13,6 +13,16 @@ def plot_and_save_hist(sims, title, out_path):
     plt.title(title)
     plt.xlabel("Cosine Similarity")
     plt.ylabel("Frequency")
+
+    # Fixing size
+    # Set fixed x-axis: 0 to 1 with steps of 0.1
+    plt.xlim(0, 1)
+    plt.xticks(np.arange(0, 1.1, 0.1))
+
+    # Set fixed y-axis: 0 to 200
+    plt.ylim(0, 200)
+    plt.yticks(np.arange(0, 201, 20))
+
     plt.tight_layout()
     # Save Figure as PNG, SVG, and PDF
     plt.savefig(f"{out_path}.png")
@@ -21,24 +31,24 @@ def plot_and_save_hist(sims, title, out_path):
     plt.close()
 
 
-def plot_results(df: pd.DataFrame) -> None:
+def plot_results(df: pd.DataFrame, especific: str = '') -> None:
     """ Plot similarity results. """
 
     out_dir = "Charts"
     plot_and_save_hist(
         df['text_vs_gt_similarity_score'],
-        title='Similarity: Text vs GT',
-        out_path=os.path.join(out_dir, 'text_vs_gt')
+        title=f'Similarity [{especific}]: Text vs GT',
+        out_path=os.path.join(out_dir, f'{especific}_text_vs_gt')
     )
 
     plot_and_save_hist(
         df['text_vs_lx_similarity_score'],
-        title='Similarity: Text vs LX',
-        out_path=os.path.join(out_dir, 'text_vs_lx')
+        title=f'Similarity [{especific}]: Text vs LX',
+        out_path=os.path.join(out_dir, f'{especific}_text_vs_lx')
     )
 
     plot_and_save_hist(
         df['gt_vs_lx_similarity_score'],
-        title='Similarity: GT vs LX',
-        out_path=os.path.join(out_dir, 'gt_vs_lx')
+        title=f'Similarity [{especific}]: GT vs LX',
+        out_path=os.path.join(out_dir, f'{especific}_gt_vs_lx')
     )
